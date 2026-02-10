864e8260febf7193492f5faa3211c5891ba93f22
Meteor2333 <Meteor23333@outlook.com>
2026-02-10 21:29:30 +0800

    删除env.d.ts的冗余内容

--- client/env.d.ts
+++ client/env.d.ts
@@ -1,16 +1,1 @@
 /// <reference types="vite/client" />
-
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue';
-  const component: DefineComponent<object, object, unknown>;
-  export default component;
-}
-
-declare module 'codemirror-editor-vue3' {
-  import type { DefineComponent } from 'vue';
-  const Codemirror: DefineComponent<{
-    modelValue?: string;
-    options?: object;
-  }>;
-  export default Codemirror;
-}
